0c7416804b73bd42b7abd1a394b5098a59950e5b
Nathaniel Erskine <racingcamel@gmail.com>
2013-09-11 19:15:54 -0400

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Poopgraph::Application.config.secret_key_base = '21f5d58ba0c344ff19da2ed22f4d4303c40dbaac5418ac6d190667fe78029da393f9172ca08f4c16174b22c0e66696153da66ac385e52474d09b337194b1a423'
